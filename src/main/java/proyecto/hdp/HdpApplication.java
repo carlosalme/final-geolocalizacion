06194620fcc6ea83196dba373a22dd303c9ab820
T-107 <T-107@PC200>
2018-04-03 21:12:47 -0500

    primer commit

---
+++ src/main/java/proyecto/hdp/HdpApplication.java
@@ -0,0 +1,12 @@
+package proyecto.hdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdpApplication.class, args);
+	}
+}
